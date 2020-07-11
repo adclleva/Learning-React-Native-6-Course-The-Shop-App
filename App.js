24f68c1e3b6a7555671fe26fecfec3cc96f8a194
adclleva <arvin.lleva@yahoo.com>
2020-07-10 20:01:50 -0700

    Created the FlatList for the ProductOverviewScreen which pulls data from the Redux store from the product

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
+
+// the boiler plate to connect the redux to the app
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
+import { useSelector } from "react-redux";
 
 const ProductOverviewScreen = (props) => {
+  // it retrieves the state as input and returns whatever we want to get from there
+  // we'll be pulling from the state of the products reducer
+  const products = useSelector((state) => state.products.availableProducts);
   return (
-    <FlatList>
-      <Text></Text>
-    </FlatList>
+    <FlatList
+      data={products} // this is the array that we'll be using
+      keyExtractor={(item) => item.id} // this is for older versions of React
+      renderItem={(itemData) => {
+        // itemData === { item, index, separators }, it gives you these props
+        return <Text>{itemData.item.title}</Text>;
+      }}
+    />
   );
 };
 
