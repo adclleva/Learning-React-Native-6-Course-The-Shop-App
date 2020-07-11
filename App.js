8bdd9bb0ce203b7188e978607e0ba9152e2c9077
adclleva <arvin.lleva@yahoo.com>
2020-07-10 21:14:11 -0700

    Added the dependencies for the navigation, added the Colors constants, created the ShopNavigator navigation stack and implemented it to our app

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
 
 // the boiler plate to connect the redux to the app
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
 import productsReducer from "./store/reducers/product";
+
+import ShopNavigator from "./navigation/ShopNavigator";
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -16,11 +17,7 @@ const store = createStore(rootReducer);
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
-        <Text>Test</Text>
-      </View>
+      <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({});
---
+++ navigation/ShopNavigator.js
@@ -0,0 +1,24 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { Platform } from "react-native";
+
+import ProductsOverViewScreen from "../screens/shop/ProductOverviewScreen";
+
+import Colors from "../constants/Colors";
+
+const ProductsNavigator = createStackNavigator(
+  {
+    ProductsOverView: ProductsOverViewScreen,
+  },
+  {
+    // the second argument is what configures the stack navigator
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primary : "",
+      },
+      headerTintColor: Platform.OS === "android" ? "fff" : Colors.primary, // colors of the title
+    },
+  }
+);
+
+export default createAppContainer(ProductsNavigator); // we need to make sure to wrap our the navigation that we'll be using
