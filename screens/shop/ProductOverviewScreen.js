1ab1106b4bce95364a0fd369ce99c758a1471c50
adclleva <arvin.lleva@yahoo.com>
2020-07-12 18:59:44 -0700

    Created the ProductItem component to display the product dummy data and added some styling to them within the page of the ProductOverviewScreen

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductOverviewScreen = (props) => {
   // it retrieves the state as input and returns whatever we want to get from there
@@ -11,8 +12,17 @@ const ProductOverviewScreen = (props) => {
       data={products} // this is the array that we'll be using
       keyExtractor={(item) => item.id} // this is for older versions of React
       renderItem={(itemData) => {
+        const { item } = itemData;
         // itemData === { item, index, separators }, it gives you these props
-        return <Text>{itemData.item.title}</Text>;
+        return (
+          <ProductItem
+            image={item.imageUrl}
+            title={item.title}
+            price={item.price}
+            onViewDetail={() => console.log(`View ${item.title} Details`)}
+            onAddToCart={() => console.log(`Added ${item.title} to cart`)}
+          />
+        );
       }}
     />
   );
