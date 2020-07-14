2afacc528fd862babeb5216551501bdf09c00980
adclleva <arvin.lleva@yahoo.com>
2020-07-13 20:29:55 -0700

    Added more details for the ProductDetailScreen which are the styles and react-native components

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -9,6 +9,7 @@ import {
 } from "react-native";
 
 import { useSelector } from "react-redux";
+import Colors from "../../constants/Colors";
 
 const ProductDetailScreen = (props) => {
   const { navigation } = props;
@@ -16,13 +17,23 @@ const ProductDetailScreen = (props) => {
 
   // this will get the slice of state which in this case is products from the productsReducer
   const selectedProduct = useSelector((state) =>
+    // this will get the product that we want
     state.products.availableProducts.find((product) => product.id === productId)
   );
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button
+          color={Colors.primary}
+          title="Add to Cart"
+          onPress={() => console.log(`Added ${selectedProduct.title} to cart`)}
+        />
+      </View>
+      <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -37,4 +48,24 @@ ProductDetailScreen.navigationOptions = (navigationData) => {
 
 export default ProductDetailScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 300,
+  },
+  price: {
+    fontSize: 20,
+    color: "#888",
+    textAlign: "center",
+    marginVertical: 20,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: "center",
+    marginHorizontal: 20,
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: "center",
+  },
+});
